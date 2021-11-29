b4ced41c7e821ae6e2c9c9c687dc268a062394d2
Alejandro Marcano <+>
2021-11-29 02:13:46 +0100

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,3 +1,50 @@
+# class PlantsController < ApplicationController
+# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+#   # GET /plants
+#   def index
+#     plants = Plant.all
+#     render json: plants
+#   end
+
+#   # GET /plants/:id
+#   def show
+#     plant = Plant.find_by(id: params[:id])
+#     render json: plant
+#   end
+
+#   # POST /plants
+#   def create
+#     plant = Plant.create(plant_params)
+#     render json: plant, status: :created
+#   end
+
+#   def update
+#     plant = find_plant
+#     plant.update(plant_params)
+#     render json: plant
+#   end
+
+#   def delete
+#     plant = find_plant
+#     plant.destroy
+#     head :no_content
+#   end
+
+#   private
+
+#   def find_plant
+#     Plant.find(params[:id])
+#   end
+
+#   def render_not_found_response
+#     render json: {error: "Plant not found"}, status: :not_found
+#   end
+
+#   def plant_params
+#     params.permit(:name, :image, :price, :is_in_stock)
+#   end
+# end
+
 class PlantsController < ApplicationController
 
   # GET /plants
@@ -16,6 +63,20 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
   end
 
   private
